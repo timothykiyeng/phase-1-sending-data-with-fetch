be6ad5263edea738ead6f398aab435368e126607
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-09-02 14:55:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+
+function submitData(name, email){
+
+    let configObj = {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json", 
+            "Accept": "application/json"
+        }, 
+        body: JSON.stringify({name, email})
+    }
+    return fetch("http://localhost:3000/users", configObj)
+    .then((resp) => resp.json())
+    .then(obj => {
+        document.body.innerHTML = obj.id
+      
+    }).catch((err) => {
+        document.body.innerHTML = err.message
+    })
+}
